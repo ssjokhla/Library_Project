e8180fde94c8d92f507636d48b84eec4452eb473
cpacosta <christian.acosta1225@gmail.com>
2019-04-28 15:31:15 -0400

    Add files via upload

---
+++ Insert Books.sql
@@ -0,0 +1,273 @@
+-- Hunger Games
+INSERT INTO Publisher
+VALUES ("1111", "Publisher 1", "123 Address Ln");
+
+INSERT INTO Document 
+VALUES ("1111", "The Hunger Games", "2005-10-20", "1111");
+
+INSERT INTO Branch
+VALUES ("1111", "Library1", "Newark" );
+
+INSERT INTO Copy
+VALUES ("1111", "1", "1111", "South");
+
+INSERT INTO Author
+VALUES ("1111", "Suzanne Collins");
+
+INSERT INTO Book
+VALUES ('1111', '998877');
+
+INSERT INTO Writes
+VALUES ("1111", "1111");
+
+-- Great Gatsby
+INSERT INTO Publisher
+VALUES ("1112", "Publisher 2", "456 Address Ln");
+
+INSERT INTO Document 
+VALUES ("1112", "The Great Gatsby", "1950-10-20", "1112");
+
+INSERT INTO Copy
+VALUES ("1112", "1", "1111", "South");
+
+INSERT INTO Author
+VALUES ("1112", "F. Scott Fitzgerald");
+
+INSERT INTO Book
+VALUES ('1112', '998876');
+
+INSERT INTO Writes
+VALUES ("1112", "1112");
+	
+-- Moby Dick
+INSERT INTO Publisher
+VALUES ("1113", "Publisher 3", "789 Address Ln");
+
+INSERT INTO Document 
+VALUES ("1113", "Moby Dick", "1930-10-20", "1113");
+
+INSERT INTO Branch
+VALUES ("1112", "Library2", "Jersey City" );
+
+INSERT INTO Copy
+VALUES ("1113", "1", "1112", "South");
+
+INSERT INTO Author
+VALUES ("1113", "Herman Melville");
+
+INSERT INTO Book
+VALUES ('1113', '998878');
+
+INSERT INTO Writes
+VALUES ("1113", "1113");
+
+-- Hamlet
+INSERT INTO Publisher
+VALUES ("1114", "Publisher 4", "987 Address Ln");
+
+INSERT INTO Document 
+VALUES ("1114", "Hamlet", "1700-10-20", "1114");
+
+INSERT INTO Branch
+VALUES ("1113", "Library3", "Bayonne" );
+
+INSERT INTO Copy
+VALUES ("1114", "1", "1113", "South");
+
+INSERT INTO Author
+VALUES ("1114", "William Shakespear");
+
+INSERT INTO Book
+VALUES ('1114', '998879');
+
+INSERT INTO Writes
+VALUES ("1114", "1114");
+
+-- War and Peace
+INSERT INTO Publisher
+VALUES ("1115", "Publisher 5", "654 Address Ln");
+
+INSERT INTO Document 
+VALUES ("1115", "War and Peace", "1200-10-20", "1115");
+
+INSERT INTO Branch
+VALUES ("1115", "Library5", "Nutley" );
+
+INSERT INTO Copy
+VALUES ("1115", "1", "1115", "South");
+
+INSERT INTO Author
+VALUES ("1115", "Leo Tolstoy");
+
+INSERT INTO Book
+VALUES ('1115', '998889');
+
+INSERT INTO Writes
+VALUES ("1115", "1115");
+
+-- The Oddyssey
+INSERT INTO Publisher
+VALUES ("1116", "Publisher 6", "321 Address Ln");
+
+INSERT INTO Document 
+VALUES ("1116", "The Odyssey", "1950-10-20", "1116");
+
+INSERT INTO Branch
+VALUES ("1116", "Library6", "Clifton" );
+
+INSERT INTO Copy
+VALUES ("1116", "1", "1116", "South");
+
+INSERT INTO Author
+VALUES ("1116", "Homer");
+
+INSERT INTO Book
+VALUES ('1116', '998888');
+
+INSERT INTO Writes
+VALUES ("1116", "1116");
+
+-- The Iliad
+INSERT INTO Document 
+VALUES ("1117", "The Iliad", "1950-10-20", "1116");
+
+INSERT INTO Copy
+VALUES ("1117", "1", "1116", "South");
+
+INSERT INTO Book
+VALUES ('1117', '998887');
+
+INSERT INTO Writes
+VALUES ("1116", "1117");
+
+
+-- Alice's Adventures in Wonderland
+INSERT INTO Publisher
+VALUES ("1118", "Publisher 8", "159 Address Ln");
+
+INSERT INTO Document 
+VALUES ("1118", "Alice's Adventures in Wonderland", "1862-10-20", "1118");
+
+INSERT INTO Branch
+VALUES ("1117", "Library7", "Teaneck" );
+
+INSERT INTO Copy
+VALUES ("1118", "1", "1117", "South");
+
+INSERT INTO Author
+VALUES ("1118", "Lewis Carroll");
+
+INSERT INTO Book
+VALUES ('1118', '998886');
+
+INSERT INTO Writes
+VALUES ("1118", "1118");
+
+
+-- Pride and Prejudice
+INSERT INTO Publisher
+VALUES ("1119", "Publisher 9", "685 Address Ln");
+
+INSERT INTO Document 
+VALUES ("1119", "Pride and Prejudice", "1765-10-20", "1119");
+
+INSERT INTO Branch
+VALUES ("1119", "Library9", "Lodi" );
+
+INSERT INTO Copy
+VALUES ("1119", "1", "1119", "South");
+
+INSERT INTO Author
+VALUES ("1119", "Jane Auston");
+
+INSERT INTO Book
+VALUES ('1119', '998885');
+
+INSERT INTO Writes
+VALUES ("1119", "1119");
+
+
+-- The Catcher in the Rye
+INSERT INTO Publisher
+VALUES ("1110", "Publisher 10", "354 Address Ln");
+
+INSERT INTO Document 
+VALUES ("1110", "The Catcher in the Rye", "1945-10-20", "1110");
+
+INSERT INTO Copy
+VALUES ("1110", "1", "1119", "South");
+
+INSERT INTO Author
+VALUES ("1110", "J.D. Salinger");
+
+INSERT INTO Book
+VALUES ('1110', '998884');
+
+INSERT INTO Writes
+VALUES ("1110", "1110");
+
+
+-- 1984
+INSERT INTO Publisher
+VALUES ("1121", "Publisher 10", "741 Address Ln");
+
+INSERT INTO Document 
+VALUES ("1121", "1984", "1954-10-20", "1121");
+
+INSERT INTO Branch
+VALUES ("1121", "Library11", "Branch Brook" );
+
+INSERT INTO Copy
+VALUES ("1121", "1", "1121", "South");
+
+INSERT INTO Author
+VALUES ("1121", "George Orwell");
+
+INSERT INTO Book
+VALUES ('1121', '998884');
+
+INSERT INTO Writes
+VALUES ("1121", "1121");
+
+
+-- Catch-22
+INSERT INTO Publisher
+VALUES ("1122", "Publisher 11", "852 Address Ln");
+
+INSERT INTO Document 
+VALUES ("1122", "Catch-22", "1961-10-20", "1122");
+
+INSERT INTO Copy
+VALUES ("1122", "1", "1121", "South");
+
+INSERT INTO Author
+VALUES ("1122", "Joseph Heller");
+
+INSERT INTO Book
+VALUES ('1122', '998883');
+
+INSERT INTO Writes
+VALUES ("1122", "1122");
+
+
+-- The Grapes of Wrath
+INSERT INTO Publisher
+VALUES ("1123", "Publisher 12", "963 Address Ln");
+
+INSERT INTO Document 
+VALUES ("1123", "The Grapes of Wrath", "1400-10-20", "1123");
+
+INSERT INTO Branch
+VALUES ("1123", "Library12", "Morrisetown" );
+
+INSERT INTO Copy
+VALUES ("1123", "1", "1123", "South");
+
+INSERT INTO Author
+VALUES ("1123", "John Steinbeck");
+
+INSERT INTO Book
+VALUES ('1123', '998883');
+
+INSERT INTO Writes
+VALUES ("1123", "1123");
