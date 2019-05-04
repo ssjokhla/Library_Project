29f8066bfeb80170900c45e114a695224e58c04b
cpacosta <christian.acosta1225@gmail.com>
2019-05-04 19:47:32 -0400

    Update Checkout.php

--- HTML/Checkout.php
+++ HTML/Checkout.php
@@ -1,13 +1,13 @@
 <?php
 include("methods.php");
 session_start();
-
+echo "HI"
+$BorNO=$_POST['BorNO'];
 $DocID=$_POST['DocID'];
 $ReaderID=$_POST['ReaderID'];
 $CopyNO=$_POST['CopyNO'];
 $LibID=$_POST['LibID'];
 
-docCheckout($ReaderID, $DocID, $CopyNO, $LibID);
-
+docCheckout($BorNO, $ReaderID, $DocID, $CopyNO, $LibID);
 
 ?>
