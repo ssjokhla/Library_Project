261855056a89badd54392caf7de7605fd58e8fef
cpacosta <christian.acosta1225@gmail.com>
2019-05-04 18:19:40 -0400

    Create Checkout.php

---
+++ HTML/Checkout.php
@@ -0,0 +1,12 @@
+<?php
+include('methods.php');
+session_start();
+
+$DocID=$_POST['DocID'];
+$ReaderID=$_POST['ReaderID'];
+$CopyNO=$_POST['CopyNO'];
+$LibID=$_POST['LibID'];
+
+docCheckout($ReaderID, $DocID, $CopyNO, $LibID);
+
+?>
