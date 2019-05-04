10a988075a03dca22f4ebf98886abbaae3cc8fbe
cpacosta <christian.acosta1225@gmail.com>
2019-05-04 16:47:16 -0400

    Create searchResult.php

---
+++ HTML/searchResult.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1>Results Page</h1>
+<?php
+include('methods.php')
+session_start();
+
+$ReaderID=$_SESSION['CardNumber'];
+$DocID=$_POST['DocID'];
+$Title=$_POST['Title'];
+$PubName=$_POST['Pubname'];
+
+$con = mysqli_connect("localhost", "admin", "password", "Library");
+mysqli_select_db($con, "Library");
+
+if (!$con){
+	logError("Connection Failed: " . mysqli_connect_error());
+	die("Connection failed: " . mysqli_connect_error());
+}
+
+$query = "SELECT *
+FROM document
+NATURAL JOIN copy
+NATURAL JOIN publisher
+WHERE Title = '$Title'
+OR docID = '$DocID'
+or PubName = 'PubName';"
+
+$SearchResult = mysqli_query($con, $query);
+
+echo $SerachResult;
