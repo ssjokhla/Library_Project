0f15d08c027706dba1299c01b1aaa188ca97ab96
cpacosta <christian.acosta1225@gmail.com>
2019-05-04 18:50:49 -0400

    Testing without method

--- HTML/Checkout.php
+++ HTML/Checkout.php
@@ -1,5 +1,5 @@
 <?php
-include("methods.php");
+//include("methods.php");
 session_start();
 
 $DocID=$_POST['DocID'];
@@ -7,6 +7,12 @@ $ReaderID=$_POST['ReaderID'];
 $CopyNO=$_POST['CopyNO'];
 $LibID=$_POST['LibID'];
 
-docCheckout($ReaderID, $DocID, $CopyNO, $LibID);
+$currDT = NOW();
+	
+$con = mysqli_connect("localhost", "admin", "password", "Library");
+mysqli_select_db($con, "Library");
+	
+$query = "INSERT into Borrows ('ReaderID', 'DocID', 'CopyNO', 'LibID', 'BDTime') VALUES ('$ReaderID', '$DocID', '$VopyNO', '$LibID', '$currDT')";	
+mysqli_query($con, $query);
 
 ?>
