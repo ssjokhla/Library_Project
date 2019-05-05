f52946543b60d68565b9ca11b84c42ffb9e55bd2
cpacosta <christian.acosta1225@gmail.com>
2019-05-05 12:30:21 -0400

    Create Reserve.php

---
+++ HTML/Reserve.php
@@ -0,0 +1,18 @@
+<?php
+include("methods.php");
+session_start();
+
+$ResNO=$_POST['ResNO'];
+echo "ResNo   : ".$ResNO;
+$DocID=$_POST['DocID'];
+echo "DocID   : ".$DocID;
+$ReaderID=$_SESSION['CardNumber'];
+echo "ReaderID: ".$ReaderID;
+$CopyNO=$_POST['CopyNO'];
+echo "CopyNO  : ".$CopyNO;
+$LibID=$_POST['LibID'];
+echo "LibID   : ".$LibID;
+
+docReturn($ResNO, $ReaderID, $DocID, $CopyNO, $LibID);
+
+?>
