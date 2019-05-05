3737617ae1cc24894986f9d925a9a29b5d1c6f0b
cpacosta <christian.acosta1225@gmail.com>
2019-05-05 15:33:17 -0400

    Create addDocument.php

---
+++ HTML/addDocument.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Add Book</h2>
+<form action="/searchResult.php" method="post">
+Publisher ID:<br>
+<input type="text" name="pubPubID">
+<br><br>
+Publisher Name:<br>
+<input type="text" name="pubName">
+<br><br>
+Publisher Address:<br>
+<input type="text" name="pubAddress">
+<br><br>
+Document ID:<br>
+<input type="text" name="docID">
+<br><br>
+Document Title:<br>
+<input type="text" name="docTitle">
+<br><br>
+Document Publisher Date:<br>
+<input type="text" name="docPDate">
+<br><br>
+Library Branch ID:<br>
+<input type="text" name="branchLibID">
+<br><br>
+Library Name:<br>
+<input type="text" name="branchLName">
+<br><br>
+brancLLocation:<br>
+<input type="text" name="branchLLocation">
+<br><br>
+<input type="submit" value="Add Book">
+</form>
