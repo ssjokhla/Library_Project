2c36cd5bb9c7d321b85b54eb5b5f17235914a7c5
cpacosta <christian.acosta1225@gmail.com>
2019-05-05 11:50:12 -0400

    Testing ReaderID Session

--- HTML/Checkout.php
+++ HTML/Checkout.php
@@ -1,10 +1,10 @@
 <?php
 include("methods.php");
 session_start();
-echo "HI";
+
 $BorNO=$_POST['BorNO'];
 $DocID=$_POST['DocID'];
-$ReaderID=$_POST['ReaderID'];
+$ReaderID=$_SESSION['CardNumber'];
 $CopyNO=$_POST['CopyNO'];
 $LibID=$_POST['LibID'];
 
