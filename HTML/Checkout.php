90cba4e093a33c47e51d1d697051882f46d3faee
cpacosta <christian.acosta1225@gmail.com>
2019-05-05 12:03:40 -0400

    Troubleshooting docCheckout

--- HTML/Checkout.php
+++ HTML/Checkout.php
@@ -3,10 +3,15 @@ include("methods.php");
 session_start();
 
 $BorNO=$_POST['BorNO'];
+echo "BorNo   : ".$BorNO;
 $DocID=$_POST['DocID'];
+echo "DocID   : ".$DocID;
 $ReaderID=$_SESSION['CardNumber'];
+echo "ReaderID: ".$ReaderID
 $CopyNO=$_POST['CopyNO'];
+echo "CopyNO  : ".$CopyNO;
 $LibID=$_POST['LibID'];
+echo "LibID   : ".$LibID;
 
 docCheckout($BorNO, $ReaderID, $DocID, $CopyNO, $LibID);
 
