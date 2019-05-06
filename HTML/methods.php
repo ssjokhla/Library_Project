4bc20943143593843be2839524ac2d115a5d3308
ssjokhla <ssjokhla@gmail.com>
2019-05-06 22:41:55 +0000

    Fixed most popular books by year

--- HTML/methods.php
+++ HTML/methods.php
@@ -548,7 +548,7 @@ function computeAverageFine($Bornumber, $readerID, $BDTime, $RDTime)
 
 function mostPopularYear($year)
 {
-$con = mysqli_connect("localhost", "admin", "password", "Library");
+	$con = mysqli_connect("localhost", "admin", "password", "Library");
         mysqli_select_db($con, "Library");
 
         if (!$con)
@@ -557,26 +557,26 @@ $con = mysqli_connect("localhost", "admin", "password", "Library");
         }
 
 
-        $query = "Select count(BRW.DocID) as cnt, B.LNAME as 'Library', D.Title From Borrows BRW JOIN Book BK ON BRW.DocID = BK.DocID JOIN Document D ON D.DocID = BK.DocID JOIN Branch B ON B.LibID = BRW.LibID WHERE YEAR(BRW.BDTime) = '2019' GROUP BY BRW.DocID, YEAR(BRW.BDTime),Library.B.Lname Order By cnt DESC LIMIT 10";
+	$query = "Select count(BRW.DocID) as cnt, B.LNAME as 'Library', D.Title From Borrows BRW JOIN Book BK ON BRW.DocID = BK.DocID JOIN Document D ON D.DocID = BK.DocID JOIN Branch B ON B.LibID = BRW.LibID WHERE YEAR(BRW.BDTime) = '$year' GROUP BY BRW.DocID, YEAR(BRW.BDTime),Library.B.Lname Order By cnt DESC LIMIT 10";
 
-        $SearchResult = mysqli_query($con, $query);
-
-        $rowCount = mysqli_num_rows($SearchResult);
-        if (mysqli_num_rows($rowCount) != 0)
+	$result = mysqli_query($con, $query);
+        $rowCount = mysqli_num_rows($result);
+        echo "<br> Row count is: ".$rowCount;
+        if (mysqli_num_rows($result) != 0)
         {
-                echo "result /=0<br>";
                 echo "<table>";
-                echo"<tr><th>Count</th><th>LibraryID</th></tr><tr><th>Title</th></tr>";
-                while($rows = mysqli_fetch_array($SearchResult,MYSQLI_ASSOC))
+                echo "<tr><th>Count</th><th>LibraryID</th><th>Reader Name</th></tr>";
+                while($rows = mysqli_fetch_array($result,MYSQLI_ASSOC))
                 {
-                        echo "<tr><td>".$rows['cnt']."</td></tr>";
-                        echo "<tr><td>".$rows['Library']."</td></tr>";
-                        echo "<tr><td>".$rows['Title']."</td></tr>";
+                        echo "<tr><td>".$rows['cnt']."</td>";
+                        echo "<td>".$rows['Library']."</td>";
+                        echo "<td>".$rows['Title']."</td>";
 
                 }
         echo "</table>";
         }
-	
+
+
 }
 
 ?>
