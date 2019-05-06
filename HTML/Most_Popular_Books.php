04085dda3f7c002c8a6b5d3913021986781e3ef8
cpacosta <christian.acosta1225@gmail.com>
2019-05-05 23:54:11 -0400

    Update and rename Most_Popular_Books.html to Most_Popular_Books.php

---
+++ HTML/Most_Popular_Books.php
@@ -0,0 +1,11 @@
+<h1>Most Borrowed Books</h1>
+<?php
+include('methods.php');
+$con = mysqli_connect("localhost", "admin", "password", "Library");
+mysqli_select_db($con, "Library");
+if (!$con){
+	logError("Connection Failed: " . mysqli_connect_error());
+	die("Connection failed: " . mysqli_connect_error());
+}
+frequentBorrowedBooks()
+?>
