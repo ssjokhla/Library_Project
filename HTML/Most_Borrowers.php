271eed7afc2e77f73ec32f4956c5de3320190f4a
cpacosta <christian.acosta1225@gmail.com>
2019-05-05 23:45:16 -0400

    Update Most_Borrowers.php

--- HTML/Most_Borrowers.php
+++ HTML/Most_Borrowers.php
@@ -2,11 +2,13 @@
 <h1>Most Borrows</h1>
 <?php
 include('methods.php');
+
+$LibID=$_POST['LibID'];
 $con = mysqli_connect("localhost", "admin", "password", "Library");
 mysqli_select_db($con, "Library");
 if (!$con){
 	logError("Connection Failed: " . mysqli_connect_error());
 	die("Connection failed: " . mysqli_connect_error());
 }
-frequentBorrowers()
+frequentBorrowers($LibID);
 ?>
