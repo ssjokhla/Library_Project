ade2fd5e2fda8cbbc75ae2bc49146c8a1d98d65a
ssjokhla <ssjokhla@gmail.com>
2019-05-06 04:50:36 +0000

    searchResult working

--- HTML/searchResult.php
+++ HTML/searchResult.php
@@ -59,8 +59,8 @@ echo "Our Row count is: ".$rowCount;
 #var_dump(getType(urlICS($rows['Name'], $rows['Location'], $rows['Description'], $rows['Google_Time_Start'], $rows['Google_Time_End'])));
 if (mysqli_num_rows($SearchResult) != 0)
 {
-	echo "Inside If Statement";
-	echo "result /=0<br>";
+//	echo "Inside If Statement";
+//	echo "result /=0<br>";
 	echo "<table>";
 	echo"<tr><th>Borrow ID</th><th>Document ID</th><th>Copy Number</th></tr>";
 	while($rows = mysqli_fetch_array($SearchResult,MYSQLI_ASSOC))
